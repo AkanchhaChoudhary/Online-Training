626279d8613a849c3a2bbba053aa9fc72a3f2212
AkanchhaChoudhary <akanchha.ece@gmail.com>
2020-03-09 23:18:42 +0100

    Added Harvard - CS50 - Course to the repo

---
+++ Harvard - CS50 - Course/Computer Science/week1/prog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+int score1=get_int("score1:");
+
+
+
+   printf("score1:");
+   for (int i = 0; i<score1; i++)
+   {
+     printf("#");
+   }
+   
+   printf("\n");
+   
+}
+
